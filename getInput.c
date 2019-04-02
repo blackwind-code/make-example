cf1940bb280592b4807ec008bc37bd52c89a2608
KyungWonPark <kw.park.person@gmail.com>
2019-04-02 18:54:26 +0000

    asdf

---
+++ getInput.c
@@ -0,0 +1,8 @@
+#include "getInput.h"
+
+void getInput(void)
+{
+	printf("Press Enter to continue\n");
+	int c = getchar();
+	return;
+}
---
+++ main.c
@@ -0,0 +1,9 @@
+#include "hello.h"
+#include "getInput.h"
+
+int main(void)
+{
+	hello();
+	getInput();
+	return 0;
+}
